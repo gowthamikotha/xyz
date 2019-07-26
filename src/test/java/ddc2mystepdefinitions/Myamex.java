bdfb5a9efaca64c2d93cf1e3c621c7b02a9e5429
training_d2.03.07 <training_d2.03.07@accenture.com>
2019-07-26 17:57:17 +0530

    ghasfjhsda

---
+++ src/test/java/ddc2mystepdefinitions/Myamex.java
@@ -0,0 +1,88 @@
+package ddc2mystepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Myamex {
+
+@Given("I have account")
+public void i_have_account() {
+    // Write code here that turns the phrase above into concrete actions
+//    throw new cucumber.api.PendingException();
+	System.out.println("I have account");
+}
+
+@Given("I will provide the data")
+public void i_will_provide_the_data() {
+    // Write code here that turns the phrase above into concrete actions
+//    throw new cucumber.api.PendingException();
+	System.out.println("I will provide the data");
+
+}
+
+@Then("My login sucess")
+public void my_login_sucess() {
+    // Write code here that turns the phrase above into concrete actions
+//    throw new cucumber.api.PendingException();
+	System.out.println("My login sucess");
+
+}
+
+@Given("I have logged in")
+public void i_have_logged_in() {
+    // Write code here that turns the phrase above into concrete actions
+//    throw new cucumber.api.PendingException();
+	System.out.println("I have logged in");
+
+}
+
+@Then("I click on reward points")
+public void i_click_on_reward_points() {
+    // Write code here that turns the phrase above into concrete actions
+//    throw new cucumber.api.PendingException();
+	System.out.println("I click on reward points");
+
+}
+
+@Then("I will check for my rewards")
+public void i_will_check_for_my_rewards() {
+    // Write code here that turns the phrase above into concrete actions
+//    throw new cucumber.api.PendingException();
+	System.out.println("I will check for my rewards");
+
+}
+
+@Given("I am offered with referral bonus")
+public void i_am_offered_with_referral_bonus() {
+    // Write code here that turns the phrase above into concrete actions
+//    throw new cucumber.api.PendingException();
+	System.out.println("I am offered with referral bonus");
+
+}
+
+@When("I share the link with my friend")
+public void i_share_the_link_with_my_friend() {
+    // Write code here that turns the phrase above into concrete actions
+//    throw new cucumber.api.PendingException();
+	System.out.println("I share the link with my friend");
+
+}
+
+@Then("I will get some zeta points")
+public void i_will_get_some_zeta_points() {
+    // Write code here that turns the phrase above into concrete actions
+//    throw new cucumber.api.PendingException();
+	System.out.println("I will get some zeta points");
+
+}
+
+@Then("I will enjoy my friday party")
+public void i_will_enjoy_my_friday_party() {
+    // Write code here that turns the phrase above into concrete actions
+//    throw new cucumber.api.PendingException();
+	System.out.println("I will enjoy my friday party");
+
+}
+
+}
---
+++ src/test/java/gowthami_runners/Myrunner.java
@@ -0,0 +1,16 @@
+package gowthami_runners;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+
+@RunWith(Cucumber .class)
+@CucumberOptions(features="MYDEMODDC2FEATURES",glue="ddc2mystepdefinitions",tags= {"@Regression","@Smoke"},
+plugin={"html:target/cucumber-htmlreport.html"})
+
+public class Myrunner {
+	
+
+}
